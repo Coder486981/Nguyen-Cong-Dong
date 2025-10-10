0287410d419d62b7776b4bf142ec2c2daf40ade1
Nguyen Cong Dong <congdong12346@gmail.com>
2025-10-10 23:41:12 +0700

    update: xóa hàm nhan(), giữ sum()

--- main.cpp
+++ main.cpp
@@ -5,10 +5,17 @@ int sum(int n){
     int s = 0;
     for (int i = 1; i <= n; ++i)   
         s += i;                 
-    return s;                     
+    return s;
 }
+int nhan(int m){
+    int a=1;
+    if (m <= 1) return 1;
+    return m * nhan(m - 1);
+}                     
+
 int main(){
     int n = 5;
     cout << "Sum = " << sum(n) << endl; //1+2+3+4+5=15;
+    cout << "Nhan = " << nhan(n) << endl; //5!=120
     return 0;
 }

b95c6512a67905e944d4146072ece1b884e5dfcc
Coder486981 <164005000+Coder486981@users.noreply.github.com>
2025-10-10 23:21:13 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int sum(int n){
     int s = 0;
-    for (int i = 1; i <= n; ++i)   
+    for (int i = 1; i < n; ++i)   
         s += i;                 
     return s;                     
 }
